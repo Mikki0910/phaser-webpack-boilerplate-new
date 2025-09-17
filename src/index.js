5895fb18facc9184dad1e2d188fe73ee82de9777
Mykola Kalchuk <mykola.kalchuk@unity3d.com>
2025-09-17 17:01:43 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,28 +1,54 @@
-
 import Phaser from "phaser";
 
 const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
+    // WebGL(Web graphics library) JS Api fro rendering 2D and 3D graphics
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        // Arcade physics plugin, manages physics simulation
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 200
+            }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update
     }
-  },
-  scene: {
-    preload: preload,
-    create: create
-  }
-};
+}
+
+let bird = null;
+let totalDelta = 0;
+let delta = 0;
+
+
+//Loading assets, such as images, music, animations...
+function preload() {
+    // 'this' context - scene
+    //containas functions and properties we can use
+    this.load.image("sky", "./assets/sky.png");
+    this.load.image("bird", "./assets/bird.png");
+}
+
+function create() {
+    //x, y, key
+    this.add.image(0, 0, "sky").setOrigin(0, 0);
+    bird = this.physics.add.sprite(config.width / 10, config.height / 2, "bird").setOrigin(0);
+}
+
+//60 fps
+function update() {
+
+    if (totalDelta >= 1000) {
+        console.log(bird.body.velocity.y);
+        totalDelta = 0;
+    }
+
+    totalDelta += delta;
+}
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
