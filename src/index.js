dab6859240757e624c671389cb8cf1933c85cbe6
Mykola Kalchuk <mykola.kalchuk@unity3d.com>
2025-09-18 12:50:20 +0200

    Render Multiple Pipes

--- src/index.js
+++ src/index.js
@@ -20,12 +20,11 @@ const config = {
 const FLAP_VELOCITY = 250;
 const initialBirdPosition = {x: config.width / 10, y: config.height / 2};
 const pipeVerticalDistanceRange = [150, 250];
-let pipeVerticalDistance = Phaser.Math.Between(pipeVerticalDistanceRange[0], pipeVerticalDistanceRange[1]);
-let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+const PIPES_TO_RENDER = 4;
+let pipeHorizontalDistance = 0;
 let bird = null;
 let upperPipe = null;
 let lowerPipe = null;
-
 
 
 function preload() {
@@ -38,27 +37,35 @@ function create() {
     this.add.image(0, 0, "sky").setOrigin(0, 0);
     bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, "bird").setOrigin(0);
     bird.body.gravity.y = 400;
-    upperPipe = this.physics.add.sprite(400, pipeVerticalPosition, "pipe").setOrigin(0, 1);
-    lowerPipe = this.physics.add.sprite(400, upperPipe.y + pipeVerticalDistance, "pipe").setOrigin(0, 0);
+
+    for (let i = 0; i < PIPES_TO_RENDER; i++) {
+        pipeHorizontalDistance += 400;
+        let pipeVerticalDistance = Phaser.Math.Between(pipeVerticalDistanceRange[0], pipeVerticalDistanceRange[1]);
+        let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+        upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, "pipe").setOrigin(0, 1);
+        lowerPipe = this.physics.add.sprite(pipeHorizontalDistance, upperPipe.y + pipeVerticalDistance, "pipe").setOrigin(0, 0);
+
+        upperPipe.body.velocity.x = -200;
+        lowerPipe.body.velocity.x = -200;
+    }
+
     this.input.on('pointerdown', flap, this);
     this.input.keyboard.on('keydown-SPACE', flap, this);
 }
 
-function flap()
-{
+function flap() {
     debugger
     bird.body.velocity.y = -FLAP_VELOCITY;
 }
 
 
 function update() {
-    if(bird.y > config.height || bird.y < -config.height) {
+    if (bird.y > config.height || bird.y < -config.height) {
         restartPlayerPosition();
     }
 }
 
-function restartPlayerPosition()
-{
+function restartPlayerPosition() {
     bird.x = initialBirdPosition.x;
     bird.y = initialBirdPosition.y;
     bird.body.velocity.y = 0;
